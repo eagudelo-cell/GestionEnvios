c1aee9100d28cfb7be3ce8259ab2bae1c69f24d5
eagudelo-cell <eagudelo@gmail.com>
2026-04-30 19:06:54 -0500

    cambio2

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "gestion_envios");
+$conn = new mysqli("mysql-edwinagudelo.alwaysdata.net", "root", "", "gestion_envios");
 
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
